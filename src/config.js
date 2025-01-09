77c8d245d88d4fbaebd900cf11a4f551f804aac5
samuelrolandumg <smartinezl2@miumg.edu.gt>
2025-01-08 19:02:56 -0600

    first commit1

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const { config } = require('dotenv');
+
+config()
+
+module.exports = {
+    host: process.env.DB_HOST || "localhost",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_DATABASE || "Clinica",
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql", // Agrega esta líneasa
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+};
